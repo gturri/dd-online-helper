c4b7e7840ca314bd57445c9741d0a02a7a3db3d2
Guillaume Turri <guillaume.turri@gmail.com>
2023-03-08 18:33:46 +0100

    Add generate script

--- generate.sh
+++ generate.sh
@@ -1,1 +1,19 @@
-curl https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.3.0/openapi-generator-cli-6.3.0.jar > bin/openapi-generator-cli.jar
+#!/bin/bash
+if [ -L "$0" ] && [ -x $(which readlink) ]; then
+	THIS_FILE="$(readlink -mn "$0")"
+else
+	THIS_FILE="$0"
+fi
+THIS_DIR="$(dirname "$THIS_FILE")"
+BIN_DIR="$THIS_DIR/bin"
+OAG_JAR="$BIN_DIR/openapi-generator-cli.jar"
+GIT_USER_ID=gturri
+GIT_PROJECT=dd-online-helper
+
+if [ ! -e "$OAG_JAR" ]; then
+	echo "Downloading openapi-generator-cli jar"
+	curl https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/6.3.0/openapi-generator-cli-6.3.0.jar > "$OAG_JAR"
+fi
+
+echo "Generating the server module"
+java -jar "$OAG_JAR" generate --git-user-id "$GIT_USER" --git-repo-id "$GIT_PROJECT" -i "$THIS_DIR/openapi.yaml" -g php-symfony -o php-server-bundle
