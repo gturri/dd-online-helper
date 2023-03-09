3ee1245b8281c01a83da7c5cef7e9935d741f856
Guillaume Turri <guillaume.turri@gmail.com>
2023-03-09 21:09:48 +0100

    Add Doctrine auto-generated migration file
    generated with "./bin/console make:migration"

---
+++ php-server/migrations/Version20230309200836.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230309200836 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE message (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, timestamp DATETIME NOT NULL --(DC2Type:datetimetz_immutable)
+        , message VARCHAR(2000) NOT NULL, room VARCHAR(255) NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE message');
+    }
+}
