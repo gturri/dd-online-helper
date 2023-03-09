8f109d34ce425d3cda44ac20e96f935d3a2bc521
Guillaume Turri <guillaume.turri@gmail.com>
2023-03-09 21:05:15 +0100

    Add Doctrine auto-generated entity
    generated with "./bin/console make:entity"

---
+++ php-server/src/Entity/Message.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MessageRepository::class)]
+class Message
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE)]
+    private ?\DateTimeImmutable $timestamp = null;
+
+    #[ORM\Column(length: 2000)]
+    private ?string $message = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $room = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTimestamp(): ?\DateTimeImmutable
+    {
+        return $this->timestamp;
+    }
+
+    public function setTimestamp(\DateTimeImmutable $timestamp): self
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getRoom(): ?string
+    {
+        return $this->room;
+    }
+
+    public function setRoom(string $room): self
+    {
+        $this->room = $room;
+
+        return $this;
+    }
+}
