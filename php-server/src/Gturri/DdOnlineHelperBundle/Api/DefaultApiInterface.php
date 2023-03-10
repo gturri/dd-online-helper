271bcbf410ac65d11cdc3e01967fddf4c614ed6f
Guillaume Turri <guillaume.turri@gmail.com>
2023-03-10 21:44:22 +0100

    implement dicePost endpoint

--- php-server/src/Gturri/DdOnlineHelperBundle/Api/DefaultApiInterface.php
+++ php-server/src/Gturri/DdOnlineHelperBundle/Api/DefaultApiInterface.php
@@ -7,9 +7,36 @@ use OpenAPI\Server\Api\DefaultApiInterface;
 use OpenAPI\Server\Model\DicePostRequest;
 use OpenAPI\Server\Model\LastEventsGet200ResponseInner;
 
+use App\Entity\Message;
+use Doctrine\ORM\EntityManagerInterface;
+
 class DefaultApi implements DefaultApiInterface {
+	public function __construct(EntityManagerInterface $entityManager) {
+		$this->entityManager = $entityManager;
+	}
+
 	public function dicePost(?DicePostRequest $dicePostRequest, int &$responseCode, array &$responseHeaders): void {
-		// TODO
+		$message = new Message();
+		$message->setRoom($dicePostRequest->getRoom());
+		$message->setTimestamp(new \DateTimeImmutable());
+		$message->setMessage($this->generateDiceMessage($dicePostRequest));
+
+		$this->entityManager->persist($message);
+		$this->entityManager->flush();
+	}
+
+	private function generateDiceMessage(DicePostRequest $request) {
+		$result = "{$request->getPlayer()} threw dice:\n";
+
+		foreach($request->getDice() as $dice){
+			$result .= "{$dice->getNumberOfDice()}D{$dice->getNumberOfSides()}:";
+			for($d=0 ; $d < $dice->getNumberOfDice() ; $d++ ){
+				$result .= mt_rand(1, $dice->getNumberOfSides()) . ";";
+			}
+			$result .= "\n";
+		}
+
+		return $result;
 	}
 
 
