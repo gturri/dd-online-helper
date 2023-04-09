999aa1f4f7895b862067dae256d762ee90a8acf5
Guillaume Turri <guillaume.turri@gmail.com>
2023-04-09 20:13:31 +0200

    Add simple call from the front

--- ddOnlineHelper-angularFront/src/app/app.component.ts
+++ ddOnlineHelper-angularFront/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
+@Injectable()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ddOnlineHelper-angularFront';
+	client: HttpClient;
+	data: any;
+
+	constructor(private http: HttpClient) {
+		this.client = http;
+		this.getData();
+	}
+
+	getData() {
+		console.log("tempGT: in getData");
+		this.client.get("/last-events?room=myroom")
+			.subscribe(data => {
+					this.data = data
+					console.log("tempGT: got " + data);
+					setTimeout(() => {this.getData()}, 1000);
+					});
+
+	}
 }
