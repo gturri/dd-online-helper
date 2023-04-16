c7d8acece3362140b11b9ba06e326984a39cab63
Guillaume Turri <guillaume.turri@gmail.com>
2023-04-16 20:45:36 +0200

    Fix indentation of app.module.ts

--- ddOnlineHelper-angularFront/src/app/app.module.ts
+++ ddOnlineHelper-angularFront/src/app/app.module.ts
@@ -5,27 +5,29 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { StartComponent } from './start/start.component';
 
 export function apiConfigFactory (): Configuration {
-  let href = window.location.href;
-  const params: ConfigurationParameters = {
-    basePath: href.substr(0, href.length-1) // remove trailing slash
-  }
-  return new Configuration(params);
+	let href = window.location.href;
+	const params: ConfigurationParameters = {
+basePath: href.substr(0, href.length-1) // remove trailing slash
+	}
+	return new Configuration(params);
 }
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    ApiModule.forRoot(apiConfigFactory),
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		StartComponent
+	],
+	imports: [
+		BrowserModule,
+		ApiModule.forRoot(apiConfigFactory),
+		HttpClientModule,
+		FormsModule,
+		ReactiveFormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
