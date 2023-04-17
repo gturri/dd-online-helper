8833715afd0fc89aa24fa8fea4bce89defafea36
Guillaume Turri <guillaume.turri@gmail.com>
2023-04-17 21:39:48 +0200

    Retrieve player and room from the form

--- ddOnlineHelper-angularFront/src/app/welcome/welcome.component.ts
+++ ddOnlineHelper-angularFront/src/app/welcome/welcome.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { CoordinateService } from '../coordinate.service';
 
+@Injectable()
 @Component({
 	selector: 'app-welcome',
 	templateUrl: './welcome.component.html',
 	styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+	welcomeForm = this.formBuilder.group({
+		player: '',
+		room: ''
+	});
 
+	constructor(
+		private formBuilder: FormBuilder,
+		private router: Router,
+		private coordinateService: CoordinateService
+	) { }
+
+	onSubmit(): void {
+		let formValues = this.welcomeForm.value;
+		if (!formValues.player){
+			alert("Player name must be filled");
+			return;
+		}
+		if (!formValues.room){
+			alert("Room must be filled");
+			return;
+		}
+
+		this.coordinateService.setPlayer(formValues.player);
+		this.coordinateService.setRoom(formValues.room);
+
+		this.router.navigate(['room']);
+
+	}
 }
