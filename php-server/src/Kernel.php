98c8d04da016b95def630ab50332a0fbf2d732eb
Guillaume Turri <guillaume.turri@gmail.com>
2023-04-20 12:18:06 +0200

    Can access directly /room/:roomId

--- php-server/src/Kernel.php
+++ php-server/src/Kernel.php
@@ -4,8 +4,30 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class Kernel extends BaseKernel
 {
-    use MicroKernelTrait;
+	use MicroKernelTrait;
+
+	public function handle(Request $request, int $type = HttpKernelInterface::MAIN_REQUEST, bool $catch = true): Response {
+		if($this->isRequestToBeHandledByTheFront($request)) {
+			// TODO: this does not feel right: reading and returning a "public" asset feels like
+			// something Symfony knows how to do, and that this code is reinventing the wheel...
+			$content = file_get_contents($this->getProjectDir() . "/public/index.html");
+			$response = new Response($content);
+			$response->prepare($request);
+			return $response;
+		}
+		return parent::handle($request, $type, $catch);
+	}
+
+	// TODO: it would probably be cleaner to instead move api endpoints behind some "/api" prefix
+	// so this function would not have to be updated every time another type of url is to be handled
+	// by the front
+	private function isRequestToBeHandledByTheFront(Request $request): bool {
+		return str_starts_with($request->server->get("PATH_INFO"), "/room");
+	}
 }
