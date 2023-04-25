a621de5f43d911d83f15e7dc7ba88f128f15bee0
Guillaume Turri <guillaume.turri@gmail.com>
2023-04-25 17:16:43 +0200

    Add a 2nd test
    try stuff to avoid code duplication, but that does not feel very
    clean. Perhaps I'll end up finding some better practices in the doc

--- ddOnlineHelper-angularFront/cypress/e2e/spec.cy.ts
+++ ddOnlineHelper-angularFront/cypress/e2e/spec.cy.ts
@@ -1,11 +1,28 @@
+const server = "http://localhost:4200";
 describe('template spec', () => {
-	it('passes', () => {
-		cy.visit('http://localhost:4200')
-		cy.get("#player").type("toto");
-		cy.get("#room").type("myroom");
-		cy.get(".button").click();
+	it('Can go to a room as a player', () => {
+		cy.visit(server)
+		isWelcomePage(cy);
 
+		moveToRoomAsPlayer(cy, "toto", "myroom");
 		cy.contains("toto");
 		cy.contains("myroom");
+	}),
+
+	it('Is redirected to welcome page if player is unknown', () => {
+		cy.visit(server + '/room/myroom')
+		isWelcomePage(cy);
 	})
 })
+
+function isWelcomePage(cy) {
+	cy.url().should('equal', server + "/");
+	cy.get("#player");
+	cy.get("#room");
+}
+
+function moveToRoomAsPlayer(cy, player: string, room: string) {
+	cy.get("#player").type(player);
+	cy.get("#room").type(room);
+	cy.get(".button").click();
+}
