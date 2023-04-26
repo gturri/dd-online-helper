3cec70bcaf82c5ea2d15d0dc48db53858d3b4c77
Guillaume Turri <guillaume.turri@gmail.com>
2023-04-26 12:34:02 +0200

    leverage cypress built-in baseUrl conf
    instead of reinventing the wheel

--- ddOnlineHelper-angularFront/cypress/e2e/spec.cy.ts
+++ ddOnlineHelper-angularFront/cypress/e2e/spec.cy.ts
@@ -1,7 +1,6 @@
-const server = "http://localhost:4200";
 describe('template spec', () => {
 	it('Can go to a room as a player', () => {
-		cy.visit(server)
+		cy.visit('')
 		isWelcomePage(cy);
 
 		moveToRoomAsPlayer(cy, "toto", "myroom");
@@ -10,32 +9,32 @@ describe('template spec', () => {
 	}),
 
 	it('Is redirected to welcome page if player is unknown', () => {
-		cy.visit(server + '/room/myroom')
+		cy.visit('/room/myroom')
 		isWelcomePage(cy);
 	}),
 
 	it('Can go directly to any room if user is already known', () => {
-		cy.visit(server);
+		cy.visit('');
 		moveToRoomAsPlayer(cy, "titi", "myroom");
 
-		cy.visit(server + '/room/anyroom');
+		cy.visit('/room/anyroom');
 		cy.contains("titi");
 		cy.contains("anyroom");
 	})
 
 	it('Has player field pre-filled on welcome page if user is already known', () => {
-		cy.visit(server);
+		cy.visit('');
 		// Pre-condition
 		cy.get('[data-cy="player"]').should('not.have.value', 'titi');
 		moveToRoomAsPlayer(cy, "titi", "myroom");
 
-		cy.visit(server);
+		cy.visit('');
 		cy.get('[data-cy="player"]').should('have.value', 'titi');
 	})
 })
 
 function isWelcomePage(cy) {
-	cy.url().should('equal', server + "/");
+	cy.url().should('equal', cy.config("baseUrl") + "/");
 	cy.get("#player");
 	cy.get("#room");
 }
