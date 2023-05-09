b309df4bcb84f73c12cb56cd450145091126848e
Guillaume Turri <guillaume.turri@gmail.com>
2023-05-09 09:15:30 +0200

    Add custom controller

--- php-server/src/Kernel.php
+++ php-server/src/Kernel.php
@@ -13,14 +13,14 @@ class Kernel extends BaseKernel
 	use MicroKernelTrait;
 
 	public function handle(Request $request, int $type = HttpKernelInterface::MAIN_REQUEST, bool $catch = true): Response {
-		if($this->isRequestToBeHandledByTheFront($request)) {
-			// TODO: this does not feel right: reading and returning a "public" asset feels like
-			// something Symfony knows how to do, and that this code is reinventing the wheel...
-			$content = file_get_contents($this->getProjectDir() . "/public/index.html");
-			$response = new Response($content);
-			$response->prepare($request);
-			return $response;
-		}
+		//if($this->isRequestToBeHandledByTheFront($request)) {
+		//	// TODO: this does not feel right: reading and returning a "public" asset feels like
+		//	// something Symfony knows how to do, and that this code is reinventing the wheel...
+		//	$content = file_get_contents($this->getProjectDir() . "/public/index.html");
+		//	$response = new Response($content);
+		//	$response->prepare($request);
+		//	return $response;
+		//}
 		return parent::handle($request, $type, $catch);
 	}
 
